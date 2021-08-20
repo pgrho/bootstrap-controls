1487eba1e75497b9454d4eef25ad6206c190b968
pgrho <shipwreck@live.jp>
2021-08-20 14:55:31 +0900

    Added Shipwreck.BootstrapControls.Blazor

---
+++ src/Shipwreck.BootstrapControls.Blazor/BootstrapCheckBox.razor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Shipwreck.BootstrapControls.Blazor
+{
+    public partial class BootstrapCheckBox : ComponentBase
+    {
+        private static int _NewId;
+        private readonly string _Id = "checkbox--" + (++_NewId);
+
+        [Parameter]
+        public string Header { get; set; }
+
+        private bool _IsChecked;
+
+        [Parameter]
+        public bool IsChecked
+        {
+            get => _IsChecked;
+            set
+            {
+                if (_IsChecked != value)
+                {
+                    _IsChecked = value;
+                    StateHasChanged();
+                    IsCheckedChanged?.Invoke(_IsChecked);
+                }
+            }
+        }
+
+        [Parameter]
+        public Action<bool> IsCheckedChanged { get; set; }
+
+        private bool _IsEnabled = true;
+
+        [Parameter]
+        public bool IsEnabled
+        {
+            get => _IsEnabled;
+            set
+            {
+                if (_IsEnabled != value)
+                {
+                    _IsEnabled = value;
+                    StateHasChanged();
+                }
+            }
+        }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object> AdditionalAttributes { get; set; }
+    }
+}
